e33816d0d874c13333678c2568ae8902dd0ff72d
Harshal Lele <frost17121997@gmail.com>
2018-04-30 12:13:51 +0530

    added app bar menu items, and a filter dialog which is shown when filter button is pressed

--- app/src/main/java/com/hllabs/linuxnews/MainActivity.kt
+++ app/src/main/java/com/hllabs/linuxnews/MainActivity.kt
@@ -1,12 +1,76 @@
 package com.hllabs.linuxnews
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
+
+
+    var currentCheckedArray:BooleanArray = BooleanArray(16 , init = {true})
+
+    var selectedSites:ArrayList<String> = Sites().siteUrls
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item != null) {
+            if(item.itemId == R.id.action_filter) {
+                showFilterDialog()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun showFilterDialog(){
+
+        val builder = AlertDialog.Builder(this@MainActivity)
+
+        builder.setTitle("Filter News Sources")
+
+
+        builder.setPositiveButton("OK" , {dialogInterface, i ->
+            val dialogView = dialogInterface as AlertDialog
+            selectedSites.clear()
+            for(i in 0 until currentCheckedArray.size){
+                currentCheckedArray[i] = dialogView.listView.isItemChecked(i)
+
+                if(currentCheckedArray[i]){
+                    selectedSites.add(Sites().siteUrls[i])
+                }
+
+            }
+
+            Log.d("LOG!" , selectedSites.size.toString())
+            Log.d("LOG!" , selectedSites.toString())
+
+
+            dialogInterface.dismiss()
+
+        })
+
+        builder.setMultiChoiceItems(Sites().siteNames, currentCheckedArray , { dialogInterface, i, b -> })
+
+        // Finally, make the alert dialog using builder
+        val dialog: AlertDialog = builder.create()
+
+        // Display the alert dialog on app interface
+        dialog.show()
+
+    }
+
+
 }
---
+++ app/src/main/java/com/hllabs/linuxnews/Sites.kt
@@ -0,0 +1,27 @@
+package com.hllabs.linuxnews
+
+class Sites{
+    val siteNames:Array<CharSequence> = arrayOf("Omgubuntu" , "bits.debian.org" , "Slashdot Linux" , "Linux.com" ,
+            "Linux Insider" , "Planet Gentoo" , "Arch Linux News" , "Lubuntu Blog" ,
+            "Ubuntu Mate Blog" , "Kubuntu Blog" , "Ubuntu Insights" , "r/Linux" ,
+            "r/opensource" , "r/ubuntu" , "r/archlinux" , "r/debian"
+    )
+    val siteUrls:ArrayList<String> = arrayListOf("http://feeds.feedburner.com/d0od" ,
+                                        "https://bits.debian.org/feeds/feed.rss" ,
+                                        "http://rss.slashdot.org/Slashdot/slashdotMain",
+                                        "https://www.linux.com/feeds/rss",
+                                        "https://www.linuxinsider.com/perl/syndication/rssfull.pl",
+                                        "https://planet.gentoo.org/rss20.xml",
+                                        "https://www.archlinux.org/feeds/news/",
+                                        "https://lubuntu.me/feed/",
+                                        "https://ubuntu-mate.org/rss.xml",
+                                        "https://kubuntu.org/feed/",
+                                        "https://insights.ubuntu.com/feed",
+                                        "https://www.reddit.com/r/linux/.rss",
+                                        "https://www.reddit.com/r/opensource/.rss",
+                                        "https://www.reddit.com/r/ubuntu/.rss",
+                                        "https://www.reddit.com/r/archlinux/.rss",
+                                        "https://www.reddit.com/r/debian/.rss"
+    )
+
+}
