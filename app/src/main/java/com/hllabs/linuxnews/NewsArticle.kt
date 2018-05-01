ef4c8c4cdabb782438d1d0c517a9a79ad645c9e5
Harshal Lele <frost17121997@gmail.com>
2018-05-01 12:23:07 +0530

    added feedUrl and siteName fields

--- app/src/main/java/com/hllabs/linuxnews/NewsArticle.kt
+++ app/src/main/java/com/hllabs/linuxnews/NewsArticle.kt
@@ -1,6 +1,9 @@
 package com.hllabs.linuxnews
 
-class Article(
-            var title:String , var author:String , var description:String , var content:String ,
-            var imgLink:String , var link:String , var pubDate:String , var categories:String
-    )
+import java.util.*
+
+data class NewsArticle(
+            var feedUrl:String = "", var siteName:String = "", var title:String = "", var author:String = "",
+            var description:String = "", var content:String = "", var imgLink:String = "", var link:String = "",
+            var pubDate: Date = Date(), var categories:List<String> = arrayListOf()
+        )
