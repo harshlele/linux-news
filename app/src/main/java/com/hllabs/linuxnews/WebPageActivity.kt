b1b54e3172999b9ca81741a376f634bd29b76a04
Harshal Lele <frost17121997@gmail.com>
2018-05-03 23:15:01 +0530

    added a few css rules for descriptions

--- app/src/main/java/com/hllabs/linuxnews/WebPageActivity.kt
+++ app/src/main/java/com/hllabs/linuxnews/WebPageActivity.kt
@@ -2,11 +2,14 @@ package com.hllabs.linuxnews
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.webkit.WebChromeClient
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_web_page.*
 
 class WebPageActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +20,70 @@ class WebPageActivity : AppCompatActivity() {
             webview.settings.javaScriptEnabled = true
             webview.webChromeClient = WebChromeClient()
             webview.webViewClient = WebViewClient()
-
             if(article != null) {
 
                 if(article.content == "") {
                     webview.loadUrl(article.link)
                 }
                 else{
-                    webview.loadData(article.content, "text/html" , "UTF-8")
+                    btnLoadFullPage.visibility = View.VISIBLE
+                    loadContent(article)
                 }
 
             }
+
+            btnLoadFullPage.setOnClickListener(object : View.OnClickListener{
+                override fun onClick(p0: View?) {
+                    webview.loadUrl(article.link)
+                    btnLoadFullPage.visibility = View.GONE
+                }
+
+            })
         }
+    }
+
+
+    fun loadContent(article: NewsArticle){
+
+        val t = article.title
+        val a = article.siteName
+        var htmlContent = """
+                        |               <html>
+                        |                   <head>
+                        |                       <style>
+                        |                           img{
+                        |                               display: block;
+                        |                               margin-left: auto;
+                        |                               margin-right: auto;
+                        |                               width: 100%
+                        |
+                        |                           }
+                        |                           div,p,body,code{
+                        |                               margin-top: 15px;
+                        |                               margin-bottom: 15px;
+                        |                               padding-top:15px;
+                        |                               padding-bottom:15px
+                        |                           }
+                        |                           h3{
+                        |                               margin-top: 15px;
+                        |                               margin-bottom: 15px;
+                        |                           }
+                        |                           h4{
+                        |                               color:grey
+                        |                           }
+                        |
+                        |                       </style>
+                        |                   </head>
+                        |                   <body>
+                        |                      <h3>$t</h3>
+                        |                      <h4>$a</h4>
+                        |
+                        |                   """.trimMargin()
+
+        htmlContent+=article.content
+        htmlContent+="</body></html>"
+
+        webview.loadData(htmlContent, "text/html" , "UTF-8")
+
     }
 }
