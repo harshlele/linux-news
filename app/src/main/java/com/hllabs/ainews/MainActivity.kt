93a6651ca6ae85afebffc2809c7515c46222e0e7
Harshal Lele <frost17121997@gmail.com>
2018-10-06 11:28:08 +0530

    added privacy policy link,changed versionCode to 2

--- app/src/main/java/com/hllabs/ainews/MainActivity.kt
+++ app/src/main/java/com/hllabs/ainews/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hllabs.ainews
 
 import android.content.Intent
+import android.net.Uri
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -226,6 +227,12 @@ class MainActivity : AppCompatActivity() {
                 startActivity(intent)
             }
             else if(item.itemId == R.id.action_ad_settings) showConsentDialog()
+
+            else if(item.itemId == R.id.action_privacy_policy){
+                val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://hllabs.github.io/linuxnews/privacy_policy"))
+                startActivity(i)
+
+            }
 
         }
 
